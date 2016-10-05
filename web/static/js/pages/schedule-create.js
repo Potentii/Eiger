e44f4a9e7f1eda45f4e0540a4239a3de1a9762ae
DeeehTO <d.sakakif@gmail.com>
2016-10-04 23:24:45 -0300

    Documentation done

--- web/static/js/pages/schedule-create.js
+++ web/static/js/pages/schedule-create.js
@@ -1,5 +1,6 @@
-// *When navigate the page Schedule Create:
+// *When navigate to the page Schedule Create:
 spa.onNavigate('schedule-create', (page, params) => {
+   // *When the user submit the form:
    $('#schedule-create-form').submit((e) => {
 
       // *The default action of the event will not be triggered:
@@ -7,19 +8,22 @@ spa.onNavigate('schedule-create', (page, params) => {
       // *Getting the key and the token:
       let auth = getAuthentication();
 
-      // *Retrieves the values of the fields 'Reason', 'Start Date', 'Start Time', 'End Date' and 'End Time':
+      // *Retrieving the values of the all fields:
       let text_reason = $('#schedule-create-reason').val();
       let date_startdate = $('#schedule-create-start-date').val();
       let time_starttime = $('#schedule-create-start-time').val();
       let date_enddate = $('#schedule-create-end-date').val();
       let time_endtime = $('#schedule-create-end-time').val();
+
+      // *Joining date and time:
       let start_date_time = date_startdate + ' ' + time_starttime;
       let end_date_time = date_enddate + ' ' + time_endtime;
+
+      // TODO Replace the ids with the actual value:
       let user_id = 1;
       let vehicle_id = 1;
-      console.log(start_date_time);
-      console.log(end_date_time);
-      // *Sends to the server 'Reason', 'Start Date', 'Start Time', 'End Date' and 'End Time' through the method POST:
+
+      // *Sending the insert request:
       $.ajax({
          url: 'http://localhost:3000/api/v1/schedules',
          method: 'POST',
@@ -27,14 +31,18 @@ spa.onNavigate('schedule-create', (page, params) => {
          data: JSON.stringify({id_vehicle_fk: vehicle_id, id_user_fk: user_id, reason: text_reason, start_date: start_date_time, end_date: end_date_time}),
          headers: {'Access-Token': auth.token, 'Access-Key': auth.key}
       }).done((data, textStatus, xhr) => {
+         // *Sending user to index page:
          spa.navigateTo('');
-         console.log('Schedule Done');
       }).fail((xhr, textStatus, err) => {
          console.log(textStatus);
       });
    });
 });
 
+
+
+// *When the user left the page:
 spa.onUnload('schedule-create', (page)=> {
+   // *Removing the submit listeners from the form:
    $('#schedule-create-form').off('submit');
 });
