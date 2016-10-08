1dbb13e08c0fdd769acc364e5c8fd868bd118451
Guilherme Reginaldo Ruella <potentii@gmail.com>
2016-10-08 12:48:52 -0300

    FABs and date formatter
    - Added missing FABs
    - Added full date formatter (DD/MM/YYYY)

--- web/static/js/date-format/date-format.js
+++ web/static/js/date-format/date-format.js
@@ -20,7 +20,7 @@ const df = (function(){
 
 
    /**
-    * Formats a date as the MySQL's datetime string format (DD-MM-YYYY HH:mm:ss)
+    * Formats a date as the MySQL's datetime string format (YYYY-MM-DD HH:mm:ss)
     * @param  {Date} date  The date to format
     * @return {string}     The formatted date and time
     * @author Guilherme Reginaldo Ruella
@@ -43,7 +43,7 @@ const df = (function(){
 
 
    /**
-    * Formats a date as the MySQL's datetime string format, but only the date part (DD-MM-YYYY)
+    * Formats a date as the MySQL's datetime string format, but only the date part (YYYY-MM-DD)
     * @param  {Date} date  The date to format
     * @return {string}     The formatted date
     * @author Guilherme Reginaldo Ruella
@@ -56,6 +56,24 @@ const df = (function(){
 
       // *Returning the string:
       return `${Y}-${M}-${D}`;
+   }
+
+
+
+   /**
+   * Formats a date as DD/MM/YYYY string
+   * @param  {Date} date  The date to format
+   * @return {string}     The formatted date
+   * @author Guilherme Reginaldo Ruella
+   */
+   function asFullDate(date){
+      // *Getting the date:
+      let Y = date.getFullYear();
+      let M = pad10(date.getMonth()+1);
+      let D = pad10(date.getDate());
+
+      // *Returning the string:
+      return `${D}/${M}/${Y}`;
    }
 
 
@@ -116,7 +134,10 @@ const df = (function(){
    return {
       asMysqlDatetime: asMysqlDatetime,
       asMysqlDate: asMysqlDate,
+
+      asFullDate: asFullDate,
       asShortDate: asShortDate,
+
       asShortTime: asShortTime,
       asShorterTime: asShorterTime
    };
