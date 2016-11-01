d1fd2f957262a70c298241279a76397f6b623c0b
WCR14 <williancontirezende@gmail.com>
2016-11-01 21:31:35 -0200

    Email setting save in a file
    Now the users can:
    -Users can fill in the settings for sending e-mail

--- rest/ui/js/pages/email-settings.js
+++ rest/ui/js/pages/email-settings.js
@@ -3,43 +3,117 @@
 // *When the user navigates to this page:
 spa.onNavigate('email-settings', (page, params) => {
 
-   let settings = {
-      smtp_server: 'adasdaada',
-      smtp_port: '123',
-      user: 'asd',
-      password: '123',
-      confirmation_send_emails: false,
-      confirmation_email_account: 'dasdas@eiger.com',
-      confirmation_subject: 'asdasd',
-      confirmation_body: 'asas'
-   };
+   // *Getting module settins:
+   let load_setting = modules.get('settings');
 
-   $('#email-settings-smtp-server').val(settings.smtp_server);
+   // *Getting name file:
+   let file_name = load_setting.EMAIL_SETTINGS_FILE;
 
-   $('#email-settings-smtp-port').val(settings.smtp_port);
+   load_setting.loadSettings(file_name)
+      .then(content => {
 
-   $('#email-settings-user').val(settings.user);
+         //* Converting string to a object:
+         let file = jQuery.parseJSON(content);
 
-   $('#email-settings-password').val(settings.password);
+         // *Setting email settings smtp server:
+         $('#email-settings-smtp-server').val(file.smtp_server);
 
-   $('#email-settings-confirmation-send').prop('checked', settings.confirmation_send_emails?true:false);
+         // *Setting email settings smtp port:
+         $('#email-settings-smtp-port').val(file.smtp_port);
 
-   $('#email-settings-confirmation-email').val(settings.confirmation_email_account);
+         // *Setting email settings user:
+         $('#email-settings-user').val(file.user);
 
-   $('#email-settings-confirmation-subject').val(settings.confirmation_subject);
+         // *Setting email settings password:
+         $('#email-settings-password').val(file.password);
 
-   $('#email-settings-confirmation-body').text(settings.confirmation_body);
+         // *Setting email settings email:
+         $('#email-settings-confirmation-email').val(file.confirmation_email_account);
 
-   // *Updating MDL Textfields:
-   mdl_util.updateTextFields('#email-settings-section');
+         // *Setting email settings subject:
+         $('#email-settings-confirmation-subject').val(file.confirmation_subject);
 
-   // *Updating MDL Textfields:
-   mdl_util.updateCheckBoxes('#email-settings-section');
+         // *Setting email settings body confrmation:
+         $('#email-settings-confirmation-body').text(file.confirmation_body);
+
+         // *Updating MDL Textfields:
+         mdl_util.updateTextFields('#email-settings-section');
+
+         // *Updating MDL Textfields:
+         mdl_util.updateCheckBoxes('#email-settings-section');
+
+
+      })
+      .catch(err => {
+
+      });
+
+   // *Listening event of submit:
+   $('#email-settings-form').submit((e) => {
+      // *Preventing action default of browser happen:
+      e.preventDefault();
+      // *Calling a function to save a file:
+      saveFile();
+   })
+   // *Listening when the user choose a file:
+   $('#email-settings-confirmation-body').on('change', (e) => {
+      let nameFile = e.target.files[0].name;
+      console.log(nameFile);
+      $('#email-settings-confirmation-body-name').text(nameFile);
+   })
+
 });
+
+/**
+ * Save a file with date of email settings
+ * @author Willian Conti Rezende
+ */
+function saveFile(){
+
+// *Getting module settins:
+let save_settings = modules.get('settings');
+
+// *Getting name file:
+let file_name_save = save_settings.EMAIL_SETTINGS_FILE;
+
+   // *Getting content of inpus in string format:
+   let content_inputs = JSON.stringify({
+   smtp_server: $('#email-settings-smtp-server').val(),
+   smtp_port: $('#email-settings-smtp-port').val(),
+   user: $('#email-settings-user').val(),
+   password: $('#email-settings-password').val(),
+   confirmation_email_account: $('#email-settings-confirmation-email').val(),
+   confirmation_subject: $('#email-settings-confirmation-subject').val(),
+   confirmation_body: $('#email-settings-confirmation-body').val()
+   });
+
+   // *Saving a file:
+   save_settings.saveSettings(file_name_save, content_inputs)
+   .then(() => {
+      // *Going to logs page:
+      spa.navigateTo('log');
+   })
+   .catch(err =>{
+
+   })
+
+}
 
 
 
 // *When the user left this page:
 spa.onLeft('email-settings', (page) => {
+
+   // *Cleaning values of inputs:
+   $('#email-settings-smtp-server').val('');
+   $('#email-settings-smtp-port').val('');
+   $('#email-settings-user').val('');
+   $('#email-settings-password').val('');
+   $('#email-settings-confirmation-email').val('');
+   $('#email-settings-confirmation-subject').val('');
+   $('#email-settings-confirmation-body').val('');
+   $('#email-settings-confirmation-body').val('');
+
+
 
 });
