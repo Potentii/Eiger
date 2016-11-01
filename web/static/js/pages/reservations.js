799247941c7ca2454cd12c0821523d5fed3631b9
Ralf Pablo Braga Soares <ralfmotard@gmail.com>
2016-11-01 00:23:56 -0200

    Commit to request update
    Updating

--- web/static/js/pages/reservations.js
+++ web/static/js/pages/reservations.js
@@ -3,6 +3,24 @@
 // *When the user navigates to this page:
 spa.onNavigate('reservations', (page, params) => {
 
+   // *Checking if the user was authenticated:
+   if(authenticated == true) {
+      // *If true:
+
+      request.getReservations(
+         {id: '', status: false, from_date: '2016/11/01', to_date: '2016/11/01'},
+         {order_by: 'id', order_type: 'asc'})
+         .done(data => {
+            console.log(data);
+         })
+         .fail(xhr => {
+            // *Checking if the request's status is 401, sending the user to the login page if it is:
+            if(xhr.status === 401){
+               spa.navigateTo('login');
+               return;
+            }
+         });
+   }
 });
 
 
