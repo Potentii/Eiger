43eac28d703d38322fcfabbe18f8e4e405d0f04c
Guilherme Reginaldo Ruella <potentii@gmail.com>
2016-11-02 20:08:09 -0200

    Added file reading method

--- web/static/js/file-encoder/file-encoder.js
+++ web/static/js/file-encoder/file-encoder.js
@@ -34,8 +34,35 @@ const file_encoder = (function(){
 
 
 
+   /**
+    * Retrieves the text content of a file
+    * @param  {File} file        The File to read
+    * @param  {function} onReady The read completion callback
+    * @author Guilherme Reginaldo Ruella
+    */
+   function asText(file, onReady){
+      // *Creating a new file reader:
+      var reader = new FileReader();
+
+      // *When the file encoding gets completed:
+      reader.addEventListener('load', () => {
+         // *Calling the callback if any, and passing the encoded string:
+         if(onReady) onReady(reader.result);
+      }, false);
+
+      // *Checking if the file is set:
+      if(file){
+         // *If it is:
+         // *Starting to encode the file:
+         reader.readAsText(file);
+      }
+   }
+
+
+
    // *Exporting the module:
    return {
-      asBase64: asBase64
+      asBase64: asBase64,
+      asText: asText
    };
 })();
